bf77b5b655e9c9042a7abb82cd820bfc8f4fbae7
Nguyen Chi Dat <40425312+ChiDatNguyen@users.noreply.github.com>
2018-10-10 23:20:17 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,19 @@
 package com.company;
 
-import java.io.FileNotFoundException;
+
 
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
 
 
         DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
 
 
-        dictionaryCommandline.dictionaryBasic();
+//      dictionaryCommandline.dictionaryBasic();
 
-        dictionaryCommandline.dictionanyAdvance();
+        dictionaryCommandline.dictionaryAdvance();
         dictionaryCommandline.dictionarySeacher();
     }
 }
